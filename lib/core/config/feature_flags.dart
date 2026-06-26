c72e2d89725273a3840384a68ff77c1c0e08e064
rkddk7165 <rkddk7165@naver.com>
2026-06-26 20:30:17 +0900

    feat: disable comment editing

--- lib/core/config/feature_flags.dart
+++ lib/core/config/feature_flags.dart
@@ -2,3 +2,8 @@ const bool adsEnabled = bool.fromEnvironment(
   'ADS_ENABLED',
   defaultValue: false,
 );
+
+const bool commentEditingEnabled = bool.fromEnvironment(
+  'COMMENT_EDITING_ENABLED',
+  defaultValue: false,
+);
